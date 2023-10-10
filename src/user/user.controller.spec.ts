e9e881eb29b8bf35578a7859d77de2dc80ca064a
alunoo <alunoeduardo95@gmail.com>
2023-10-09 21:25:13 -0300

    fix: prisma auth to user

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './user.controller';
-import { AuthService } from './user.service';
+import { UserService as UserService } from './user.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -8,7 +8,7 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [UserService],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './user.service';
+import { UserService as UserService } from './user.service';
 import { AuthController } from './user.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [UserService],
 })
-export class AuthModule {}
+export class UserModule {}
