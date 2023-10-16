de95a8e8619562e7c8a4f354afd554454a4fd692
alunoo <alunoeduardo95@gmail.com>
2023-10-16 20:34:37 -0300

    refactor(auth): add error and add type in validadeUser

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { error } from 'console';
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private usersService: UserService) {}
 
-    constructor(private usersService: UserService) {}
-
-  async validateUser(email: string, password: string): Promise<any> {
+  async validateUser(
+    email: string,
+    password: string,
+  ): Promise<{
+    id: string;
+    email: string;
+    name: string;
+    picture: string;
+    admin?: boolean;
+  } | null> {
     const user = await this.usersService.findOne(email);
     if (user && user.password === password) {
       const { password, ...result } = user;
       return result;
     }
-    return null;
+    throw error("Senha incorreta!");
   }
 }
