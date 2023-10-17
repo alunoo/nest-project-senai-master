f037749d3497fd88e5b86fa149aaae6967fc3d1e
alunoo <alunoeduardo95@gmail.com>
2023-10-16 21:53:48 -0300

    feat: local.strategy and auth services

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,41 @@
-import { Injectable } from '@nestjs/common';
-import { error } from 'console';
+// import { Injectable } from '@nestjs/common';
+// import { error } from 'console';
+// import { UserService } from 'src/user/user.service';
+
+// @Injectable()
+// export class AuthService {
+//   constructor(private usersService: UserService) {}
+
+//   async validateUser(
+//     email: string,
+//     password: string,
+//   ): Promise<{
+//     id: string;
+//     email: string;
+//     name: string;
+//     picture: string;
+//     admin?: boolean;
+//   } | null> {
+//     const user = await this.usersService.findOne(email);
+//     if (user && user.password === password) {
+//       const { password, ...result } = user;
+//       return result;
+//     }
+//     throw error('Senha incorreta!');
+//   }
+// }
+
+import { Injectable, Dependencies } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/user/entities/user.entity';
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UserService) {}
+  constructor(
+    private usersService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(
     email: string,
@@ -16,11 +47,18 @@ export class AuthService {
     picture: string;
     admin?: boolean;
   } | null> {
-    const user = await this.usersService.findOne(email);
+    const user: User = await this.usersService.findOne(email);
     if (user && user.password === password) {
       const { password, ...result } = user;
       return result;
     }
-    throw error("Senha incorreta!");
+    throw Error('Senha incorreta!');
+  }
+
+  async login(user: User) {
+    const payload = { email: user.email, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
